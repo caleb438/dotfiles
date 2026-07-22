b637e2c6c7f125c5d1d39c773b5f2a525d69c99a
caleb438 <109257821+caleb438@users.noreply.github.com>
2026-07-22 01:10:07 -0500

    changed completion keybind for blink.cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,7 +15,7 @@ return {
     "saghen/blink.cmp",
     version = "*",
     opts = {
-      keymap = { preset = "default" },
+      keymap = { preset = "super-tab" },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = "mono",
