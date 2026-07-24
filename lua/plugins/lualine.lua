18dadf3b363bfa4380874bc2e3c83ed9a22de30a
caleb438 <109257821+caleb438@users.noreply.github.com>
2026-07-24 16:22:32 -0500

    added nord color scheme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,8 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
 		options = {
-			theme = require("catppuccin.utils.lualine")("mocha"),
+			-- theme = require("catppuccin.utils.lualine")("mocha"),
+			theme = "nord",
 			icons_enabled = true,
 			component_separators = { left = "|", right = "|" },
 			section_separators = { left = "", right = "" },
